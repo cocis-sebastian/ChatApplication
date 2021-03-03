739a2a1405536da1a6b94266eaa9c3027c7a66c1
cocis-sebastian <79855925+cocis-sebastian@users.noreply.github.com>
2021-03-03 20:04:44 +0200

    login-gui

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button reg_btn;
+    private Button login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reg_btn=(Button)findViewById(R.id.register_btn);
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegistration();
+
+            }
+        });
+        login_btn=(Button)findViewById(R.id.login_button);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /////////login code aici
+            }
+        });
+    }
+
+    private void openRegistration(){
+        Intent open_reg_i = new Intent(this, RegisterUser.class);
+        startActivity(open_reg_i);
     }
 }
